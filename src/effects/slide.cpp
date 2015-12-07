382c9feb2ee59d6f778920d8934d6425f7c26f63
Tadly <jan@tadly.de>
2015-12-07 19:28:34 +0100

    added ability to choose a slide direction

--- src/effects/slide.cpp
+++ src/effects/slide.cpp
@@ -31,6 +31,8 @@ EFXSlide::~EFXSlide() {
 void EFXSlide::configure(const char *key, void *value) {
     if ( strcmp( key, "slide_time_ms") == 0 )
         slide_time_ms = *(int*) value;
+    else if ( strcmp( key, "slide_direction") == 0 )
+        slide_direction = *(int*) value;
 }
 
 bool EFXSlide::render(GLuint old_texture, GLuint new_texture) {
@@ -39,6 +41,7 @@ bool EFXSlide::render(GLuint old_texture, GLuint new_texture) {
         initial = false;
         pos_current = 0.0f;
         pos_offset_ms = (get_current_time_ms() % slide_time_ms);
+        random_direction = rand() % 4;
     }
 
     if ( pos_offset_ms && pos_current < 2.0f ) {
@@ -51,19 +54,23 @@ bool EFXSlide::render(GLuint old_texture, GLuint new_texture) {
         } else
             pos_last = pos_current;
 
-        // Slide out old image
-        tl[0] = -1.0f + pos_current;
-        tr[0] =  1.0f + pos_current;
-        bl[0] = -1.0f + pos_current;
-        br[0] =  1.0f + pos_current;
-        draw_image( old_texture, tl, tr, bl, br);
-
-        // Slide in new image
-        tl[0] = -3.0f + pos_current;
-        tr[0] = -1.0f + pos_current;
-        bl[0] = -3.0f + pos_current;
-        br[0] = -1.0f + pos_current;
-        draw_image( new_texture, tl, tr, bl, br);
+        switch (slide_direction) {
+            case 0:
+                left_to_right(old_texture, new_texture);
+                break;
+            case 1:
+                right_to_left(old_texture, new_texture);
+                break;
+            case 2:
+                top_to_bottom(old_texture, new_texture);
+                break;
+            case 3:
+                bottom_to_top(old_texture, new_texture);
+                break;
+            case 4:
+                random(random_direction, old_texture, new_texture);
+                break;
+        }
 
         return false;
     }
@@ -72,3 +79,84 @@ bool EFXSlide::render(GLuint old_texture, GLuint new_texture) {
 
     return true;
 }
+
+void EFXSlide::random(int direction, GLuint old_texture, GLuint new_texture) {
+    switch (direction) {
+        case 0:
+            left_to_right(old_texture, new_texture);
+            break;
+        case 1:
+            right_to_left(old_texture, new_texture);
+            break;
+        case 2:
+            top_to_bottom(old_texture, new_texture);
+            break;
+        case 3:
+            bottom_to_top(old_texture, new_texture);
+            break;
+    }
+}
+
+void EFXSlide::left_to_right(GLuint old_texture, GLuint new_texture) {
+    // Slide out old image
+    tl[0] = -1.0f + pos_current;
+    tr[0] =  1.0f + pos_current;
+    bl[0] = -1.0f + pos_current;
+    br[0] =  1.0f + pos_current;
+    draw_image( old_texture, tl, tr, bl, br);
+
+    // Slide in new image
+    tl[0] = -3.0f + pos_current;
+    tr[0] = -1.0f + pos_current;
+    bl[0] = -3.0f + pos_current;
+    br[0] = -1.0f + pos_current;
+    draw_image( new_texture, tl, tr, bl, br);
+};
+
+void EFXSlide::right_to_left(GLuint old_texture, GLuint new_texture) {
+    // Slide out old image
+    tl[0] = -1.0f - pos_current;
+    tr[0] =  1.0f - pos_current;
+    bl[0] = -1.0f - pos_current;
+    br[0] =  1.0f - pos_current;
+    draw_image( old_texture, tl, tr, bl, br);
+
+    // Slide in new image
+    tl[0] = 1.0f - pos_current;
+    tr[0] = 3.0f - pos_current;
+    bl[0] = 1.0f - pos_current;
+    br[0] = 3.0f - pos_current;
+    draw_image( new_texture, tl, tr, bl, br);
+};
+
+void EFXSlide::top_to_bottom(GLuint old_texture, GLuint new_texture) {
+    // Slide out old image
+    tl[1] = -1.0f + pos_current;
+    tr[1] = -1.0f + pos_current;
+    bl[1] =  1.0f + pos_current;
+    br[1] =  1.0f + pos_current;
+    draw_image( old_texture, tl, tr, bl, br);
+
+    // Slide in new image
+    tl[1] = -3.0f + pos_current;
+    tr[1] = -3.0f + pos_current;
+    bl[1] = -1.0f + pos_current;
+    br[1] = -1.0f + pos_current;
+    draw_image( new_texture, tl, tr, bl, br);
+};
+
+void EFXSlide::bottom_to_top(GLuint old_texture, GLuint new_texture) {
+    // Slide out old image
+    tl[1] = -1.0f - pos_current;
+    tr[1] = -1.0f - pos_current;
+    bl[1] =  1.0f - pos_current;
+    br[1] =  1.0f - pos_current;
+    draw_image( old_texture, tl, tr, bl, br);
+
+    // Slide in new image
+    tl[1] = 1.0f - pos_current;
+    tr[1] = 1.0f - pos_current;
+    bl[1] = 3.0f - pos_current;
+    br[1] = 3.0f - pos_current;
+    draw_image( new_texture, tl, tr, bl, br);
+};
--- src/effects/slide.h
+++ src/effects/slide.h
@@ -4,18 +4,38 @@
 
 class EFXSlide: public EFXBase {
     private:
-        // CONFIG
-        int     slide_time_ms   =    2500;  // Time in milliseconds this effect has to take
+        /*!
+         * @brief Time in milliseconds the fade effect should take
+        */
+        int     slide_time_ms    =    2500;
 
-        bool    initial         =    true;  // If this is the first call for the current transition
-        GLfloat pos_last        =    0.0f;  // The last position for our image
-        GLfloat pos_current     =    0.0f;  // The current position for our image
-        int     pos_offset_ms   =    0;     // Used in combination with "fade_time_ms" to calculate the new position for the next frame
+        /*!
+         * @biref Defines the direction to slide in/out new/old images
+         * 0 = left to right
+         * 1 = right to left
+         * 2 = top to bottom
+         * 3 = bottom to top
+         * 4 = random
+         */
+        int     slide_direction  =    0;
+
+
+        bool    initial          =  true;  // If this is the first call for the current transition
+        GLfloat pos_last         =  0.0f;  // The last position for our image
+        GLfloat pos_current      =  0.0f;  // The current position for our image
+        int     pos_offset_ms    =  0;     // Used in combination with "fade_time_ms" to calculate the new position for the next frame
+        int     random_direction =  0;     // If slide_direction = 4, this is where we store the random generated value
 
         GLfloat *tl;  // The images top left corner position
         GLfloat *tr;  // The images top right corner position
         GLfloat *bl;  // The images bottom left corner position
         GLfloat *br;  // The images bottom right corner position
+
+        void random(int direction, GLuint old_texture, GLuint new_texture);
+        void left_to_right(GLuint old_texture, GLuint new_texture);
+        void right_to_left(GLuint old_texture, GLuint new_texture);
+        void top_to_bottom(GLuint old_texture, GLuint new_texture);
+        void bottom_to_top(GLuint old_texture, GLuint new_texture);
 
     public:
         EFXSlide();
